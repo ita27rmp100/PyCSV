8481e0c1f5d178faf83e4857753e0e4594db667f
Abderahim <ita27rmp100@gmail.com>
2025-02-28 18:33:10 +0100

    debug some problems

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def data(file_csv) : # a function for import data from a file csv
+def data(fc) :
     DATA = []
-    with open(file_csv) as file :
+    with open(fc) as file :
         y = file.read()
         y = list(y)
         for i in range(y.count("\n")) :
@@ -8,12 +8,12 @@ def data(file_csv) : # a function for import data from a file csv
             DATA.append("".join(z)+";")
             y = y[y.index("\n")+1:len(y)]
         return DATA
-def dataArrRows(file_csv) :
-    DATA = data(file_csv)
+def dataArrRows(fc) :
+    DATA = data(fc)
     for i in range(0,len(DATA)) :
         DATA[i] = str(DATA[i]).replace(';','').split(',')
     return DATA
-def data_vdf(fc) : # Function to vertically organize data imported from csv file
+def data_vdf(fc) : 
     DATA = data(fc)
     data_vdf_d = {DATA[0]:DATA[1:]}
     return data_vdf_d
@@ -21,9 +21,9 @@ def data_hdf(fc) : # The function horizontally organizes the data imported from 
     DATA = data(fc)
     data_hdf_d = {}
     for i in DATA :
-        data_hdf_d[i[0:i.indeDATA(";")]]=i[i.indeDATA(";")+1:]
+        data_hdf_d[i[0:i.index(";")]]=i[i.index(";")+1:]
     return data_hdf_d
-def number_of(fc,mode="c") : # for Counts the number of columns, rows and cells in a CSV file
+def number_of(fc,mode="c") : # Counts the number of columns, rows and cells in a CSV file
     DATA = data(fc)
     z = 0
     if mode == "c" :
@@ -42,7 +42,7 @@ def number_of(fc,mode="c") : # for Counts the number of columns, rows and cells 
     elif mode == "co" :
         z = len(data_hdf(fc))
     return z
-def csv_files_in(folder) :  # this function is to return the csv files in folder 
+def csv_files_in(folder) :  # return the csv files in folder 
     import os
     list_folder = os.listdir(r"{}".format(folder))
     y = []
@@ -51,21 +51,21 @@ def csv_files_in(folder) :  # this function is to return the csv files in folder
         if DATA == ".csv" :
             y.append(i)
     return y
-def creat_and_write(fc,data="\n") : # This function is for create csv file and write  in it
+def creat_and_write(fc,data="\n") : # create csv file and write  in it
     with open(r"{}".format(fc),mode="a") as file :
         file.write(data)
     print("created")
-def keyword(fc,word) : # This function to receive a keyword and then search for the row it belongs to
+def keyword(fc,word) : #receive a keyword and then search for the row it belongs to
     DATA = data(fc)
     for i in DATA :
         if word in i :
             return i 
-def indeDATA_cell(fc,cell) : # a function for return the place of cell in the file
+def index_cell(fc,cell) : #return the place of cell in the file
     d = data(fc)
-    z = keyword(fc,cell)
-    y = d.indeDATA(z)+1
-    DATA = z[0:z.indeDATA(cell)].count(";")+1
+    z = str(keyword(fc,cell))
+    y = d.index(z)+1
+    DATA = z[0:z.index(cell)].count(";")+1
     return {"DATA":DATA,"y":y}
-def clear(fc) : # a function for delete data in file csv
+def clear(fc) : #delete data in file csv
     with open(fc,mode="w") as file :
         pass
