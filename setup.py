b2f2cf30a5bc2de55e078922c119f9b11c8618ab
Abderahim <ita27rmp100@gmail.com>
2025-04-19 17:21:01 +0100

    set up the library to publish it

--- setup.py
+++ setup.py
@@ -2,27 +2,22 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
-
-VERSION = '0.0.13'
-DESCRIPTION = 'Streaming video data via networks'
-LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+VERSION = '0.0.1'
+DESCRIPTION = 'Simple library to handle with .csv files'
+LONG_DESCRIPTION = 'This library has a nine function to import the data from csv files , organize the data that you import ... etc.'
 
 # Setting up
 setup(
-    name="vidstream",
+    name="PyCSV",
     version=VERSION,
-    author="NeuralNine (Florian Dedov)",
-    author_email="<mail@neuralnine.com>",
+    author="Tayeb Abderahim Ismail",
+    author_email="tayebabderahim27@gmail.com",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
-    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    install_requires=['pandas', 'os'],
+    keywords=['python', 'csv', 'excel', 'file management'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
